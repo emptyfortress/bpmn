5b490d08eeeb5c1d3c34aa67b46340899904e8b2
Volkov Alexander <volkov.525@gmail.com>
2024-04-23 16:34:39 +0300

    add bpmn - not working

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,33 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import Modeler from 'bpmn-js/lib/Modeler'
+
+const xml = '@/stores/diagram.bpmn'
+
+const modeler = new Modeler({ container: '#canvas' })
+
+onMounted(async () => {
+	try {
+		await modeler.importXML(xml)
+		console.log('success')
+		// ...
+	} catch (err) {
+		console.log('err')
+		// err...
+	}
+})
+</script>
+
 <template lang="pug">
 q-page(padding)
-	.about
-		h1 This is an about page
+	h2 bpmn
+	div(id="canvas")
 </template>
 
-<style scoped lang="scss">
-@media (min-width: 1024px) {
-	.about {
-		color: $negative;
-	}
+<style scoped>
+#canvas {
+	width: 900px;
+	height: 700px;
+	background: #ccc;
 }
 </style>
