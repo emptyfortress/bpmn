16e249c68e7f615ff290172c2fcdeb2981f1c5de
Volkov Alexander <volkov.525@gmail.com>
2024-04-24 09:27:52 +0300

    viewer works with string

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,33 +1,60 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import Modeler from 'bpmn-js/lib/Modeler'
+// import BpmnModeler from 'bpmn-js/lib/Modeler'
+import BpmnViewer from 'bpmn-js'
+// import demo from '@/stores/demo.bpmn'
 
-const xml = '@/stores/diagram.bpmn'
+const xml = `
+<?xml version="1.0" encoding="UTF-8"?>
+<bpmn2:definitions xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" id="sample-diagram" targetNamespace="http://bpmn.io/schema/bpmn" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd">
+	<bpmn2:process id="process1567044459787" name="流程1567044459787">
+		<bpmn2:documentation>描述</bpmn2:documentation>
+		<bpmn2:startEvent id="StartEvent_1" name="开始"/>
+	</bpmn2:process>
+	<bpmndi:BPMNDiagram id="BPMNDiagram_1">
+		<bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="process1567044459787">
+			<bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1">
+				<dc:Bounds x="184" y="64" width="36" height="36"/>
+				<bpmndi:BPMNLabel>
+					<dc:Bounds x="191" y="40" width="22" height="14"/>
+				</bpmndi:BPMNLabel>
+			</bpmndi:BPMNShape>
+		</bpmndi:BPMNPlane>
+	</bpmndi:BPMNDiagram>
+<processType id="test"/></bpmn2:definitions>
+`
 
-const modeler = new Modeler({ container: '#canvas' })
-
-onMounted(async () => {
-	try {
-		await modeler.importXML(xml)
-		console.log('success')
-		// ...
-	} catch (err) {
-		console.log('err')
-		// err...
-	}
+onMounted(() => {
+	var viewer = new BpmnViewer({
+		container: '.canvas',
+	})
+	viewer
+		.importXML(xml)
+		.then(function (result) {
+			const { warnings } = result
+			console.log('success !', warnings)
+			viewer.get('canvas').zoom('fit-viewport')
+		})
+		.catch(function (err) {
+			const { warnings, message } = err
+			console.log('something went wrong:', warnings, message)
+		})
 })
 </script>
 
 <template lang="pug">
 q-page(padding)
 	h2 bpmn
-	div(id="canvas")
+	.canvas(ref="canvas")
 </template>
 
-<style scoped>
-#canvas {
+<style scoped lang="scss">
+// :deep(.bjs-powered-by) {
+// 	display: none;
+// }
+.canvas {
 	width: 900px;
 	height: 700px;
-	background: #ccc;
+	background: #fff;
 }
 </style>
