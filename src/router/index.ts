619906de6b2baa43e9ea0327fee4d42a775c7eb3
Volkov Alexander <volkov.525@gmail.com>
2024-04-25 09:57:36 +0300

    meta in route

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import NotFound from '@/views/NotFound.vue'
 
+declare module 'vue-router' {
+	interface RouteMeta {
+		title: string
+		transition: any
+	}
+}
+
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
@@ -17,6 +24,7 @@ const router = createRouter({
 			// this generates a separate chunk (About.[hash].js) for this route
 			// which is lazy-loaded when the route is visited.
 			component: () => import('@/views/BpmnView.vue'),
+			meta: { title: 'Start', transition: 'fade' },
 		},
 		{
 			path: '/bpmn/pizza',
@@ -25,10 +33,16 @@ const router = createRouter({
 			// this generates a separate chunk (About.[hash].js) for this route
 			// which is lazy-loaded when the route is visited.
 			component: () => import('@/views/BpmnView1.vue'),
+			meta: { title: 'Пицца', transition: 'fade' },
 		},
 		{ path: '/:pathMatch(.*)', component: NotFound },
 		// { path: '/:pathMatch(.*)', name: 'bad-not-found', component: NotFound },
 	],
+})
+
+const DEFAULT_TITLE = 'BPMN'
+router.beforeEach((to) => {
+	document.title = to.meta.title || DEFAULT_TITLE
 })
 
 export default router
